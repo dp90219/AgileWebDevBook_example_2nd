45576583e3a50f200a4d955f287f7992c954f9a1
liupeng <dp90219@gmail.com>
2014-04-20 19:40:53 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '9136c8f7d1bed4f7d25d73e4c2ae6f9b69a98011ebef4f3a57d006098e16ebfd171350ed13093f2b025bb0a6c282c7cd770307857549f178417181364d7bd5c4'
---
+++ note.md
@@ -0,0 +1,9 @@
+#rails new depot -d mysql
+
+## rails g scaffold product title:string description:text image_url:string price:decimal
+
+- <body class='<% controller.controller_name %>'> 
+- <tr class="<%= cycle('list_line_odd', 'list_line_even') %>">
+
+
+
