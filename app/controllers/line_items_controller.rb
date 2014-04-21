5f230f3f02aee39a0479df494e16d8435dc396e9
liupeng <dp90219@gmail.com>
2014-04-21 17:05:25 +0800

    finished chapter 11

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -58,10 +58,9 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @cart = @line_item.cart
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to @cart }
+      format.html { redirect_to line_items_path }
       format.json { head :no_content }
     end
   end
